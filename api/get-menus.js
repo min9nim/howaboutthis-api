e9f3c76466f0e3397653a67f56798a16fa086595
Keating <mgsong@madup.com>
2020-06-09 19:39:58 +0900

    add get-menus

---
+++ api/get-menus.js
@@ -0,0 +1,13 @@
+const allowCors = require('../src/utils/cors')
+const Menu = require('../src/models/menu')
+
+const getMenusHandler = async (req, res) => {
+    const docs = await Menu.find()
+    console.log(docs)
+    res.json({
+        status: 'succeeded',
+        result: docs,
+    })
+}
+
+module.exports = allowCors(getMenusHandler)
