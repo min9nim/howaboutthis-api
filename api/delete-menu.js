a2761b0aad8498c5101a43d76795e1543b519054
Keating <mgsong@madup.com>
2020-06-10 09:21:56 +0900

    update

--- api/delete-menu.js
+++ api/delete-menu.js
@@ -2,10 +2,10 @@ const allowCors = require('../src/utils/cors')
 const Menu = require('../src/models/menu')
 
 const addMenuHandler = async (req, res) => {
-    const doc = await Menu.create(req.body)
+    console.log('req.body._id', req.body._id)
+    await Menu.deleteOne({_id: req.body._id})
     res.json({
         status: 'succeeded',
-        result: doc,
     })
 }
 
--- src/models/menu.js
+++ src/models/menu.js
@@ -3,13 +3,20 @@ const mongoose = require('./dbConnect')
 
 const Schema = mongoose.Schema
 const menuSchema = new Schema({
-  title: String,
-  category: String,
   url: String,
+  title: String,
   desc: String,
   image: String,
-  createdAt: Number,
-  updatedAt: Number,
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    required: 'Must have start date - default value is the created date'
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now,
+    required: 'Must have start date - default value is the created date'
+  },
 })
 
 module.exports = mongoose.model('Menu', menuSchema, 'menus')
