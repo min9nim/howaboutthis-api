9903022bd79cec83bae2e4609b8e5bd6d11ed456
min9nim <min9nim@gmail.com>
2020-06-13 01:37:32 +0900

    throw Error when no payload

--- api/menu-to-slack.js
+++ api/menu-to-slack.js
@@ -5,19 +5,23 @@ const Menu = require('../src/models/menu')
 const axios = require('axios').default
 
 const menuToSlack = async (req, res) => {
-  let selected
-  if (req.body._id) {
-    selected = await Menu.findOne({ _id: req.body._id }).lean()
-  } else {
-    const docs = await Menu.find().lean()
-    const randomIndex = Math.floor(Math.random() * docs.length)
-    selected = docs[randomIndex]
-  }
-
-  let result
   try {
+    if (!req.body) {
+      throw Error('payload is not found')
+    }
+    let selected
+    if (req.body._id) {
+      selected = await Menu.findOne({ _id: req.body._id }).lean()
+    } else {
+      const docs = await Menu.find().lean()
+      const randomIndex = Math.floor(Math.random() * docs.length)
+      selected = docs[randomIndex]
+    }
+
+    let result
+
     const channel = req.body.channel
-    if(!channel){
+    if (!channel) {
       throw Error('channel is required')
     }
     result = await axios.post(process.env.SLACK_URL, {
