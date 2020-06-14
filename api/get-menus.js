c3b9061b30fa6b4bb5a80495a013da422aa28e4b
min9nim <min9nim@gmail.com>
2020-06-14 21:04:08 +0900

    등록일 역순 정렬

--- api/get-menus.js
+++ api/get-menus.js
@@ -4,7 +4,7 @@ const allowCors = require('../src/utils/cors')
 const Menu = require('../src/models/menu')
 
 const getMenusHandler = async (req, res) => {
-  const docs = await Menu.find()
+  const docs = await Menu.find().sort({createdAt: -1})
   // console.log(docs)
   res.json({
     status: 'succeeded',
