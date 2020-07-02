0d1628bbbb666ba35d4d653cb7488a5c412bc828
keating529 <mgsong@madup.com>
2020-07-03 08:35:32 +0900

    add Comment collection

---
+++ src/models/comment.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose')
+
+const schema = new mongoose.Schema({
+  author: String,
+  content: String,
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now,
+  }
+})
+
+module.exports = mongoose.model('Comment', schema, 'comments')
