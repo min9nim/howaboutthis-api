95396c1bfd8026fbbfcf6e256bf3365eb6400b30
keating529 <mgsong@madup.com>
2020-07-03 10:12:54 +0900

    댓글 포함해서 내리기

--- api/get-menus.js
+++ api/get-menus.js
@@ -3,14 +3,22 @@ loadEnv()
 initDB()
 
 const allowCors = require('../src/utils/cors')
+const {propEq} = require('ramda')
 const Menu = require('../src/models/menu')
+const Comment = require('../src/models/Comment')
 
 const getMenusHandler = async (req, res) => {
-  const docs = await Menu.find().sort({createdAt: -1})
+  const comments = await Comment.find().lean()
+  const docs = await Menu.find().sort({createdAt: -1}).lean()
+  const commentsAdded = docs.map(doc => {
+    doc.comments = comments.filter(propEq('menuId', doc._id))
+    return doc
+  })
+
   // console.log(docs)
   res.json({
     status: 'succeeded',
-    result: docs
+    result: commentsAdded
   })
 }
 
