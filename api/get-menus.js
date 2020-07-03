7ffcd0806db664fd0126f55df13f3e470c42cec6
keating529 <mgsong@madup.com>
2020-07-03 11:32:33 +0900

    use Promise.all

--- api/get-menus.js
+++ api/get-menus.js
@@ -8,8 +8,10 @@ const Menu = require('../src/models/menu')
 const Comment = require('../src/models/comment')
 
 const getMenusHandler = async (req, res) => {
-  const comments = await Comment.find().lean()
-  const docs = await Menu.find().sort({createdAt: -1}).lean()
+  // const comments = await Comment.find().lean()
+  // const docs = await Menu.find().sort({createdAt: -1}).lean()
+
+  const [comments, docs] = await Promise.all([Comment.find().lean(), Menu.find().sort({createdAt: -1}).lean()])
   const commentsAdded = docs.map(doc => {
     doc.comments = comments.filter(propEq('menuId', doc._id))
     return doc
